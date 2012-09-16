977bf18c0c0b47a7ed0ca30d299fe3e53dc67579
Maxime Chéramy <maxime81@gmail.com>
2012-09-16 17:30:26 +0200

    Un peu de gestion d'erreur dans DownloadPart.

--- DownloadPart.py
+++ DownloadPart.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-import ftplib
+import ftplib, socket
 from PyQt4.QtCore import pyqtSignal, QThread
 
 class DownloadPart(QThread):
@@ -28,24 +28,32 @@ class DownloadPart(QThread):
 
     def run(self):
         self.stateChanged.emit(1)
-        self.ftp.connect(str(self.url.host()), int(self.url.port(21)))
-        self.stateChanged.emit(2)
-        self.stateChanged.emit(3)
-        self.ftp.login()
-        self.stateChanged.emit(4)
 
-        self.ftp.sendcmd("TYPE I")
-        data_read = 0
-        conn = self.ftp.transfercmd('RETR ' + str(self.url.path()), rest=self._start)
-        while data_read < self.to_read and not self.canceled:
-            chunk = conn.recv(8192)
-            size = min(self.to_read - data_read, len(chunk))
-            self.localfile.write(chunk[:size])
-            data_read += size
-            self.dataTransferProgress.emit(data_read, self.to_read)
-        self.stateChanged.emit(5)
-        conn.close()
-        self.localfile.close()
-        self.done.emit(not self.canceled, self)
-        self.stateChanged.emit(0)
-
+        try:
+            self.ftp.connect(str(self.url.host()), int(self.url.port(21)))
+            self.stateChanged.emit(2)
+            self.stateChanged.emit(3)
+            self.ftp.login()
+            self.stateChanged.emit(4)
+            self.ftp.sendcmd("TYPE I")
+            data_read = 0
+            conn = self.ftp.transfercmd('RETR ' + str(self.url.path()), rest=self._start)
+            while data_read < self.to_read and not self.canceled:
+                chunk = conn.recv(8192)
+                size = min(self.to_read - data_read, len(chunk))
+                self.localfile.write(chunk[:size])
+                data_read += size
+                self.dataTransferProgress.emit(data_read, self.to_read)
+            self.stateChanged.emit(5)
+            conn.close()
+            self.localfile.close()
+            self.done.emit(not self.canceled, self)
+            self.stateChanged.emit(0)
+        except socket.error, (value, message):
+            print "erreur", message
+            self.stateChanged.emit(0)
+            self.done.emit(False, self)
+        except ftplib.error_perm, message:
+            print "erreur", message
+            self.stateChanged.emit(0)
+            self.done.emit(False, self)
